1e19e5f8eda1b18f904a5ea90fe43ad185894f36
vova <q107828@i.ua>
2016-09-28 22:50:42 +0300

    Create view of images and large part og logic (deleting and uploading)

---
+++ imageController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace  controller;
+
+require_once 'imageModel.php';
+require_once 'enterDb.php';
+
+use \database\enterDB as DB;
+use \model\imageModel as ImageModel;
+
+class imageController extends ImageModel
+{
+    /**
+     * variable for connection to database
+     *
+     * @var
+     */
+    protected $db1;
+
+    /**
+     * imageController constructor for connection to database
+     */
+    function __construct()
+    {
+        $dbObj     = new DB();
+        $this->db1 = $dbObj->connect();
+    }
+
+    /**
+     * function for uploading images
+     *
+     * @param $file
+     * @return mixed
+     */
+    public function uploadImage($file)
+    {
+        $directory = "uploadedImages/" . basename($file['image']['name']);
+        $date = date("l dS F of   h:i:s A");
+
+        parent::setFileName($file['image']['name']);
+        parent::setSize($file['image']['size']);
+        parent::setDate($date);
+
+        $allowed_filetypes = array('.jpg', '.jpeg', '.gif', '.png');
+        $ext = substr(parent::getFileName(), strpos(parent::getFileName(), '.'), strlen(parent::getFileName()) - 1);
+
+        if (!in_array($ext, $allowed_filetypes)) {
+            return "<h2>Warning: You can upload only .jpeg, .gif and .png ! Change file and try again please.</h2>";
+        } else {
+
+            if (parent::getSize() < 5 * 1024 * 1024) {
+                $stmt = $this->db1->prepare("INSERT INTO `images` (`file_name`, `size`, `date`) VALUES (:image, :size, :date)");
+                $stmt->execute(array('image' => parent::getFileName(), 'size' => parent::getSize(), 'date' => parent::getDate()));
+
+                if (move_uploaded_file($file['image']['tmp_name'], $directory)) {
+                    header('Location: index.php');
+                } else {
+                    return "<h1> Uploading failed </h1>";
+                }
+            } else {
+                return "<h2> Warning: File's size is more then 5Mb, please chose another file! </h2>";
+            }
+        }
+        return null;
+    }
+
+    /**
+     * function for deleting image
+     *
+     * @param $id
+     */
+    public function deleteImage($id)
+    {
+        parent::setId($id);
+
+        $sql2p = $this->db1->prepare("DELETE FROM `images` WHERE `id`= ? ");
+        $sql2p->execute(array(parent::getId()));
+    }
+}
--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php
 
 require_once 'enterDb.php';
+require_once 'imageController.php';
 
 use \database\enterDB as DB;
+use \controller\imageController as Logic;
 
-
+$control = new Logic();
 $dbObj   = new DB();
 $db1     = $dbObj->connect();
 
+if (!empty($_FILES['image']['name']) && isset($_POST['upload'])) {
+    $res = '';
+    $res = $control->uploadImage($_FILES);
+    if(!empty($res)) echo $res;
+
+} elseif (empty($_FILES['image']['name']) && isset($_POST['upload'])) {
+    echo "<h2> Please, choose file for uploading!</h2>";
+}
+
+if (isset($_POST['delete'])) $control->deleteImage($_POST['imageId']);
 ?>
 
 <!DOCTYPE html>
@@ -27,6 +39,26 @@ $db1     = $dbObj->connect();
             </form>
             <h2>List of file's which have been uploaded:</h2>
 
+            <?php
+            $images = $db1->query("SELECT * FROM `images`")->fetchAll();
+
+            foreach ($images as $image) {
+                    echo '<div id="img_div">';
+                    echo '<img src ="uploadedImages/' . $image['file_name'] . '">';
+                    echo '<p> Name: ' . $image['file_name'] . '</p>';
+                    echo '<p> Size: ' . $image['size'] . '</p>';
+                    echo '<p> Date and Time: ' . $image['date'] . '</p>';
+
+                    echo '<form  action="index.php" method="post" enctype="multipart/form-data">';
+
+                    echo '<p><input name="imageId" type="hidden" value="'. $image['id'] .'"></p>';
+                    echo '<p><input name="delete"  type="submit" value="Delete"></p>';
+                    echo '<a href="uploadedImages/' . $image['file_name'] . '" download>Download</a>';
+                    echo '</form>';
+                    echo '</div>';
+            }
+            ?>
+        </div>
     </body>
 </html>
 
